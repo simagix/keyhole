027b07ee14b48cf78906e67c21343c171c0640bd
Kuei-chun Chen <simagix@gmail.com>
2018-06-01 12:06:07 -0400

    Add Docker build to build script

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ mkdir -p build
 env GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-linux-x64 keyhole.go
 env GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-osx-x64 keyhole.go
 env GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-win-x64.exe keyhole.go
+
+docker build -t simagix/keyhole .
