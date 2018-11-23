b3a19bfb15fdc9d2e554b6d6df1526c9544da10e
Kuei-chun Chen <simagix@gmail.com>
2018-11-23 08:06:07 -0500

    Add decode deltas

---
+++ core/diagnostic_deltas_pub.go
@@ -0,0 +1,9 @@
+// +build public
+// Copyright 2018 Kuei-chun Chen. All rights reserved.
+
+package keyhole
+
+func (d *DiagnosticData) decodeFTDC(data []byte) error {
+	d.decodeFirstDoc(data)
+	return nil
+}
