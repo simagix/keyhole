66deeb97c4b879e05fd282c3223161eb71996e41
Kuei-chun Chen <simagix@gmail.com>
2018-12-04 17:08:21 -0500

    add ftdc unit tests

--- ftdc/metrics_test.go
+++ ftdc/metrics_test.go
@@ -10,7 +10,6 @@ import (
 func TestReadMetricsSummary(t *testing.T) {
 	var err error
 	var buffer []byte
-	filename := "../test_data/diagnostic.data/metrics.2017-10-12T20-08-53Z-00000"
 	if buffer, err = ioutil.ReadFile(filename); err != nil {
 		t.Fatal(err)
 	}
@@ -24,13 +23,25 @@ func TestReadMetricsSummary(t *testing.T) {
 func TestReadAllMetrics(t *testing.T) {
 	var err error
 	var buffer []byte
-	filename := "../test_data/diagnostic.data/metrics.2017-10-12T20-08-53Z-00000"
 	if buffer, err = ioutil.ReadFile(filename); err != nil {
 		t.Fatal(err)
 	}
 	m := NewMetrics()
 	m.ReadAllMetrics(buffer)
 	if len(m.Blocks) != len(m.Data) {
+		t.Fatal()
+	}
+}
+
+func TestReadMetrics(t *testing.T) {
+	var err error
+	var buffer []byte
+	if buffer, err = ioutil.ReadFile(filename); err != nil {
+		t.Fatal(err)
+	}
+	m := NewMetrics()
+	m.readMetrics(buffer, false)
+	if len(m.Blocks) != 164 {
 		t.Fatal()
 	}
 }
