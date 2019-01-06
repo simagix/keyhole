7978ebfaf1230b408b1b68e62a7e7b09983bc043
Kuei-chun Chen <simagix@gmail.com>
2019-01-05 21:03:45 -0500

    use replica to test

--- test.sh
+++ test.sh
@@ -19,9 +19,10 @@ echo ; echo "Spin up mongod"
 mongod --version
 mkdir -p data/db
 rm -rf data/db/*
-mongod --port 30097 --dbpath data/db --logpath data/mongod.log --fork --wiredTigerCacheSizeGB .5
+mongod --port 30097 --dbpath data/db --logpath data/mongod.log --fork --wiredTigerCacheSizeGB 1  --replSet replset
 validate "failed to start mongod"
-
+mongo --quiet mongodb://localhost:30097/admin --eval 'rs.initiate()'
+sleep 2
 mongo --port 30097 _KEYHOLE_88800 --eval "db.setProfilingLevel(0, {slowms: 10})"
 validate "failed to set profiling level"
 
@@ -34,31 +35,31 @@ validate ""
 
 # Test Info
 echo ; echo "==> Test printing cluster info (--info <uri>)"
-go run keyhole.go --info mongodb://localhost:30097/KEYHOLEDB
+go run keyhole.go --info mongodb://localhost:30097/KEYHOLEDB?replicaSet=replset
 if [ $? != 0 ]; then
     exit
 fi
 
 # Test seed
 echo ; echo "==> Test seeding default docs (--seed <uri>)"
-go run keyhole.go --seed mongodb://localhost:30097/KEYHOLEDB
+go run keyhole.go --seed mongodb://localhost:30097/KEYHOLEDB?replicaSet=replset
 validate ""
 
 echo ; echo "==> Test seeding default docs after dropping collection (--seed --drop <uri>)"
-go run keyhole.go --seed --drop mongodb://localhost:30097/KEYHOLEDB
+go run keyhole.go --seed --drop mongodb://localhost:30097/KEYHOLEDB?replicaSet=replset
 validate ""
 
 echo ; echo "==> Test seeding docs from a template (--file <file> --collection <collection> <uri>)"
-go run keyhole.go --seed --file examples/template.json --collection template mongodb://localhost:30097/KEYHOLEDB
+go run keyhole.go --seed --file examples/template.json --collection template mongodb://localhost:30097/KEYHOLEDB?replicaSet=replset
 validate ""
 
 echo ; echo "==> Test seeding docs from a template after dropping collection (--file <file> --collection <collection> --drop <uri>)"
-go run keyhole.go --seed --file examples/template.json --collection template --drop mongodb://localhost:30097/KEYHOLEDB
+go run keyhole.go --seed --file examples/template.json --collection template --drop mongodb://localhost:30097/KEYHOLEDB?replicaSet=replset
 validate ""
 
 # Test Index
 echo ; echo "==> Test printing cluster indexes (--index <uri>)"
-go run keyhole.go --index mongodb://localhost:30097/KEYHOLEDB
+go run keyhole.go --index mongodb://localhost:30097/KEYHOLEDB?replicaSet=replset
 validate ""
 
 # Test Schema
@@ -71,17 +72,17 @@ go run keyhole.go --schema --file examples/template.json
 validate ""
 
 echo ; echo "==> Test printing schema from a template (--schema --collection <collection> <uri>)"
-go run keyhole.go --schema --collection cars mongodb://localhost:30097/KEYHOLEDB
+go run keyhole.go --schema --collection cars mongodb://localhost:30097/KEYHOLEDB?replicaSet=replset
 validate ""
 
 # Test load test
 echo ; echo "==> Test load from a template (--file <file> <uri>)"
 go run keyhole.go --file examples/template.json --duration 2 \
-    --tps 300 --conn 10 --simonly mongodb://localhost:30097/KEYHOLEDB
+    --tps 300 --conn 10 --simonly mongodb://localhost:30097/KEYHOLEDB?replicaSet=replset
 validate ""
 
 go run keyhole.go --file examples/template.json --duration 3 \
-    --tps 300 --conn 10 --tx examples/transactions.json mongodb://localhost:30097/KEYHOLEDB
+    --tps 300 --conn 10 --tx examples/transactions.json mongodb://localhost:30097/KEYHOLEDB?replicaSet=replset
 validate ""
 
 # Test loginfo
