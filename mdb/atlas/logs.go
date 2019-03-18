8a873459e4f311546bf790b54ed2d9f57f3b984b
Kuei-chun Chen <simagix@gmail.com>
2019-03-17 23:49:43 -0400

    support startDate and endDate params downloading logs

--- mdb/atlas/logs.go
+++ mdb/atlas/logs.go
@@ -19,21 +19,32 @@ import (
 type Logs struct {
 	apiKey      string
 	clusterName string
+	endUnix     int64
 	err         error
 	groupID     string
+	startUnix   int64
 	userName    string
 	verbose     bool
 }
 
 // ParseAtlasURI parses atlas://user:apiKey@groupID/cluster
 func ParseAtlasURI(uri string) *Logs {
-	matched := regexp.MustCompile(`^atlas:\/\/(\S+):(\S+)@(\S+)/(\S+)`)
+	matched := regexp.MustCompile(`^atlas:\/\/(\S+):(\S+)@([^\/]+)\/([^\/]+)(\/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])))?`)
 	if matched.MatchString(uri) == false {
 		return &Logs{err: errors.New(`Incorrect format, should be "atlas://user:apiKey@groupID/clusterName"`)}
 	}
 
 	results := matched.FindStringSubmatch(uri)
-	return &Logs{userName: results[1], apiKey: results[2], groupID: results[3], clusterName: results[4]}
+	t := time.Now()
+	endUnix := t.Unix()
+	startUnix := t.Add(time.Hour * -24).Unix()
+	if len(results) > 6 && results[6] != "" {
+		t, _ = time.Parse("2006-01-02", results[6])
+		startUnix = t.Add(time.Hour * -24).Unix()
+		endUnix = t.Add(time.Hour * 24).Unix()
+	}
+	return &Logs{userName: results[1], apiKey: results[2], groupID: results[3],
+		clusterName: results[4], startUnix: startUnix, endUnix: endUnix}
 }
 
 // SetVerbose -
@@ -65,7 +76,7 @@ func (lg *Logs) DownloadLogs(dirname string) ([]string, error) {
 		if strings.Index(strings.ToLower(maps["hostname"].(string)), strings.ToLower(lg.clusterName+"-")) == 0 &&
 			strings.Index(maps["typeName"].(string), "REPLICA_") == 0 {
 			var filename string
-			if filename, err = lg.downloadLog(dirname, maps["hostname"].(string)); err == nil {
+			if filename, err = lg.downloadLog(dirname, maps["hostname"].(string), lg.startUnix, lg.endUnix); err == nil {
 				filenames = append(filenames, filename)
 			}
 		}
@@ -73,13 +84,13 @@ func (lg *Logs) DownloadLogs(dirname string) ([]string, error) {
 	return filenames, err
 }
 
-func (lg *Logs) downloadLog(dirname string, hostname string) (string, error) {
+func (lg *Logs) downloadLog(dirname string, hostname string, startUnix int64, endUnix int64) (string, error) {
 	var err error
 	var resp *http.Response
 	var b []byte
 	var filename = dirname + "/mongodb.log." + hostname + ".gz"
-
 	uri := BaseURL + "/groups/" + lg.groupID + "/clusters/" + hostname + "/logs/mongodb.gz"
+	uri += "?startDate=" + fmt.Sprintf("%v", startUnix) + "&endDate=" + fmt.Sprintf("%v", endUnix)
 	if lg.verbose {
 		fmt.Println("download from", uri)
 	}
