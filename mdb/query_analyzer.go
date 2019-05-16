2be1ae676e616806501a9edeabcb0f5b86127479
Kuei-chun Chen <simagix@gmail.com>
2019-05-16 07:23:02 -0400

    explain handles inputStages

--- mdb/query_analyzer.go
+++ mdb/query_analyzer.go
@@ -115,11 +115,6 @@ func (qa *QueryAnalyzer) GetSummary(doc bson.M) (string, error) {
 	buffer.WriteString("* All Plans Execution\n")
 	for _, execution := range allPlansExecution {
 		executionStages := execution.(bson.M)["executionStages"].(bson.M)
-		inputStage := executionStages["inputStage"].(bson.M)
-		if shardName != "" {
-			inputStage = inputStage["inputStage"].(bson.M)
-		}
-		buffer.WriteString(fmt.Sprintf(" * key pattern: %v\n", Stringify(inputStage["keyPattern"].(bson.M))))
 		buffer.WriteString(fmt.Sprintf(" ├─ totalKeysExamined: %v\n", execution.(bson.M)["totalKeysExamined"]))
 		buffer.WriteString(fmt.Sprintf(" └─ totalDocsExamined: %v\n", execution.(bson.M)["totalDocsExamined"]))
 		buffer.WriteString(fmt.Sprintf("   ├─ executionStages.stage: %v\n", executionStages["stage"]))
@@ -127,12 +122,27 @@ func (qa *QueryAnalyzer) GetSummary(doc bson.M) (string, error) {
 		buffer.WriteString(fmt.Sprintf("   ├─ executionStages.needYield: %v\n", executionStages["needYield"]))
 		buffer.WriteString(fmt.Sprintf("   ├─ executionStages.restoreState: %v\n", executionStages["restoreState"]))
 		buffer.WriteString(fmt.Sprintf("   └─ executionStages.saveState: %v\n", executionStages["saveState"]))
-		buffer.WriteString(fmt.Sprintf("     ├─ inputStage.stage: %v\n", inputStage["stage"]))
-		buffer.WriteString(fmt.Sprintf("     ├─ inputStage.executionTimeMillisEstimate: %v\n", inputStage["executionTimeMillisEstimate"]))
-		buffer.WriteString(fmt.Sprintf("     ├─ inputStage.needYield: %v\n", inputStage["needYield"]))
-		buffer.WriteString(fmt.Sprintf("     ├─ inputStage.restoreState: %v\n", inputStage["restoreState"]))
-		buffer.WriteString(fmt.Sprintf("     ├─ inputStage.saveState: %v\n", inputStage["saveState"]))
-		buffer.WriteString(fmt.Sprintf("     └─ inputStage.works: %v\n", inputStage["works"]))
+
+		var inputStages primitive.A
+		if executionStages["inputStage"] != nil {
+			inputStages = append(inputStages, executionStages["inputStage"].(bson.M))
+		} else if executionStages["inputStages"] != nil {
+			inputStages = executionStages["inputStages"].(primitive.A)
+		}
+
+		for _, input := range inputStages {
+			if input.(bson.M)["inputStage"] != nil {
+				input = input.(bson.M)["inputStage"].(bson.M)
+			}
+			inputStage = input.(bson.M)
+			buffer.WriteString(fmt.Sprintf("     ├─ key pattern: %v\n", Stringify(inputStage["keyPattern"].(bson.M))))
+			buffer.WriteString(fmt.Sprintf("     ├─ inputStage.stage: %v\n", inputStage["stage"]))
+			buffer.WriteString(fmt.Sprintf("     ├─ inputStage.executionTimeMillisEstimate: %v\n", inputStage["executionTimeMillisEstimate"]))
+			buffer.WriteString(fmt.Sprintf("     ├─ inputStage.needYield: %v\n", inputStage["needYield"]))
+			buffer.WriteString(fmt.Sprintf("     ├─ inputStage.restoreState: %v\n", inputStage["restoreState"]))
+			buffer.WriteString(fmt.Sprintf("     ├─ inputStage.saveState: %v\n", inputStage["saveState"]))
+			buffer.WriteString(fmt.Sprintf("     └─ inputStage.works: %v\n", inputStage["works"]))
+		}
 
 		buffer.WriteString("\n")
 	}
