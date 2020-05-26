fe8cee70f3e472842472d21d5014b21d24eb5da3
Kuei-chun Chen <simagix@gmail.com>
2020-05-26 08:56:12 -0400

    use docker hub build image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,13 @@
+FROM golang:alpine as builder
+RUN apk update && apk add dep git && rm -rf /var/cache/apk/* \
+  && mkdir -p /go/src/github.com/simagix/keyhole
+ADD . /go/src/github.com/simagix/keyhole
+WORKDIR /go/src/github.com/simagix/keyhole
+RUN dep ensure && go build -o keyhole-linux-x64
 FROM alpine
 MAINTAINER Ken Chen <simagix@gmail.com>
-ADD build/keyhole-linux-x64 /keyhole
+RUN addgroup -S simagix && adduser -S simagix -G simagix
+USER simagix
+WORKDIR /home/simagix
+COPY --from=builder /go/src/github.com/simagix/keyhole/keyhole-linux-x64 /
 CMD ["/keyhole", "--version"]
--- build.sh
+++ build.sh
@@ -19,11 +19,12 @@ mkdir -p build
 export ver="2.3.5"
 export version="v${ver}-$(date "+%Y%m%d")"
 env GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-osx-x64 keyhole.go
-env GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-linux-x64 keyhole.go
-env GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-win-x64.exe keyhole.go
 
-if [ "$1" == "docker" ]; then
-    docker build -t simagix/keyhole:latest -t simagix/keyhole:${ver} .
-    docker rmi -f $(docker images -f "dangling=true" -q)
+GIT=$(which git)
+if [ "$GIT" != "" ]; then
+  BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  if [ "$BRANCH" == "mongo-driver" ]; then
+    env GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-linux-x64 keyhole.go
+    env GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-win-x64.exe keyhole.go
+  fi
 fi
-#go build -o build/keyhole-osx-x64 keyhole.go
