1851af93d286ff7ac3892f549598ddd1e0acf2a9
Kuei-chun Chen <simagix@gmail.com>
2020-05-27 12:39:57 -0400

    Use Docker build as CI

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
 FROM golang:alpine as builder
-RUN apk update && apk add dep git && rm -rf /var/cache/apk/* \
+RUN apk update && apk add dep git bash && rm -rf /var/cache/apk/* \
   && mkdir -p /go/src/github.com/simagix/keyhole
 ADD . /go/src/github.com/simagix/keyhole
 WORKDIR /go/src/github.com/simagix/keyhole
-RUN dep ensure && go build -o keyhole-linux-x64
+RUN ./build.sh all
 FROM alpine
 MAINTAINER Ken Chen <simagix@gmail.com>
 RUN addgroup -S simagix && adduser -S simagix -G simagix
 USER simagix
+WORKDIR /build
+COPY --from=builder /go/src/github.com/simagix/keyhole/build/keyhole-* /build/
 WORKDIR /home/simagix
-COPY --from=builder /go/src/github.com/simagix/keyhole/keyhole-linux-x64 /keyhole
+COPY --from=builder /go/src/github.com/simagix/keyhole/build/keyhole-linux-x64 /keyhole
 CMD ["/keyhole", "--version"]
--- build.sh
+++ build.sh
@@ -18,13 +18,16 @@ $DEP ensure $UPDATE
 mkdir -p build
 export ver="2.3.5"
 export version="v${ver}-$(date "+%Y%m%d")"
-env GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-osx-x64 keyhole.go
+env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-osx-x64 keyhole.go
 
-GIT=$(which git)
-if [ "$GIT" != "" ]; then
-  BRANCH=$(git rev-parse --abbrev-ref HEAD)
-  if [ "$BRANCH" == "mongo-driver" ]; then
-    env GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-linux-x64 keyhole.go
-    env GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-win-x64.exe keyhole.go
-  fi
+if [ "$1" == "all"  ]; then
+  env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-linux-x64 keyhole.go
+  env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-win-x64.exe keyhole.go
+fi
+
+if [ "$1" == "docker"  ]; then
+  docker build . -t simagix/keyhole
+  id=$(docker create simagix/keyhole)
+  docker cp $id:/build - | tar x
+  docker rmi -f $(docker images -f "dangling=true" -q)
 fi
