3b39c86a72ee424cec8e4d9af9e844692fa093ea
Kuei-chun Chen <simagix@gmail.com>
2020-07-19 18:54:52 -0400

    refactor build

--- build.sh
+++ build.sh
@@ -14,19 +14,21 @@ if [ -d vendor ]; then
     UPDATE="-update"
 fi
 
-$DEP ensure $UPDATE
 mkdir -p build
 export ver="2.4.3"
 export version="v${ver}-$(date "+%Y%m%d")"
-env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-osx-x64 keyhole.go
 
-if [ "$1" == "cross-platform"  ]; then
+if [ "$1" == ""  ]; then
+  $DEP ensure $UPDATE
+  env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-osx-x64 keyhole.go
+elif [ "$1" == "cross-platform"  ]; then
+  $DEP ensure $UPDATE
+  env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-osx-x64 keyhole.go
   env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-linux-x64 keyhole.go
   env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$version" -o build/keyhole-win-x64.exe keyhole.go
-fi
-
-if [ "$1" == "all"  ]; then
-  docker build . -t simagix/keyhole
+elif [ "$1" == "all" ]; then
+  $DEP ensure $UPDATE
+  docker build  -f Dockerfile . -t simagix/keyhole
   id=$(docker create simagix/keyhole)
   docker cp $id:/build - | tar x
   docker rmi -f $(docker images -f "dangling=true" -q)
