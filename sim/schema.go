a04857cf67c1fd21354040c53855aa6bdeea4ad7
Kuei-chun Chen <simagix@gmail.com>
2020-08-16 11:02:13 -0400

    output json template to a file

--- sim/schema.go
+++ sim/schema.go
@@ -5,6 +5,9 @@ package sim
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
 	"regexp"
 
 	"github.com/simagix/gox"
@@ -15,21 +18,24 @@ import (
 // GetSchema returns a masked first doc of a collection
 func GetSchema(c *mongo.Collection, verbose bool) (string, error) {
 	var err error
-	var buf []byte
+	var data []byte
 	var doc bson.M
 	if err = c.FindOne(context.Background(), bson.M{}).Decode(&doc); err != nil {
 		return "", err
 	}
-	if buf, err = bson.MarshalExtJSON(doc, false, false); err != nil {
+	if data, err = bson.MarshalExtJSON(doc, false, false); err != nil {
 		return "", err
 	}
-	str := string(buf)
+	if verbose == true {
+		outdir := "./out/"
+		os.Mkdir(outdir, 0755)
+		ofile := outdir + c.Name() + ".json"
+		err = ioutil.WriteFile(ofile, data, 0644)
+		return fmt.Sprintf("json template written to %v", ofile), err
+	}
+	str := string(data)
 	re := regexp.MustCompile(`{"\$binary":{"base64":"[^"]*","subType":"(0[0-3])"}}`)
 	str = re.ReplaceAllString(str, `{"$$binary":{"base64":"","subType":"$1"}}`)
-	if verbose == true {
-		json.Unmarshal([]byte(str), &doc)
-		return gox.Stringify(doc, "", "  "), err
-	}
 	re = regexp.MustCompile(`{"\$binary":{"base64":"[^"]{24}","subType":"04"}}`)
 	str = re.ReplaceAllString(str, `"$$uuid"`)
 	re = regexp.MustCompile(`{"\$binary":{"base64":"","subType":"0([0-3])"}}`)
