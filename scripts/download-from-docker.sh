31d6e327bf4eef597ebcc1e0c769c23caf4448a7
Kuei-chun Chen <simagix@gmail.com>
2020-08-29 10:04:02 -0400

    prints downloaded files

--- scripts/download-from-docker.sh
+++ scripts/download-from-docker.sh
@@ -2,5 +2,5 @@
 docker rmi -f simagix/keyhole
 docker rmi simagix/keyhole
 id=$(docker create simagix/keyhole)
-docker cp $id:/dist - | tar x
-ls -l dist
+docker cp $id:/dist - | tar vx
+docker rm $id
