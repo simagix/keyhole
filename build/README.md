0bd702f73983771a9e174458008b370a59d7174e
Ken Chen <ken.chen@simagix.com>
2021-02-18 18:55:43 -0500

    update build and download details

--- build/README.md
+++ build/README.md
@@ -1,3 +1,3 @@
 # Download
 
-Executables are no longer available.  Please use `build.sh` to build.
+Executables are no longer available.  Please use `build.sh` to build.  Additional details are available at [Build and Download Keyhole](https://www.simagix.com/2021/02/build-and-download-keyhole_7.html).
--- mdb/pipeline_test.go
+++ mdb/pipeline_test.go
@@ -30,4 +30,11 @@ func TestMongoPipeline(t *testing.T) {
 		t.Fatal(errors.New("empty pipeline"))
 	}
 	t.Log(pipeline)
+
+	// required pipeline for CosmosDB, v3.6 compatible
+	str = `[ { "$match": { "operationType": { "$in": ["insert", "update", "replace"] } } }, { "$project": { "_id": 1, "fullDocument": 1, "ns": 1, "documentKey": 1 } } ]`
+	if pipeline = MongoPipeline(str); len(pipeline) == 0 {
+		t.Fatal(errors.New("empty pipeline"))
+	}
+	t.Log(pipeline)
 }
