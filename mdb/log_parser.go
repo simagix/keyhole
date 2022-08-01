0c0d82dc5f4aa744ff64170f3e8fa264adc9ebe0
simagix <ken.chen@simagix.com>
2022-08-01 10:13:47 -0400

    fixed reslen missing

--- mdb/log_parser.go
+++ mdb/log_parser.go
@@ -226,7 +226,6 @@ func (li *LogInfo) ParseLog(str string) (LogStats, error) {
 	filter = strings.Replace(strings.Replace(filter, "{ ", "{", -1), " }", "}", -1)
 	filter += aggStages
 	milli, _ := strconv.Atoi(ms)
-	reslen := getDocByField(str, "reslen:")
 
 	if strings.HasSuffix(ns, ".$cmd") {
 		ns = strings.TrimSuffix(ns, "$cmd")
@@ -235,8 +234,14 @@ func (li *LogInfo) ParseLog(str string) (LogStats, error) {
 		quote := strings.Index(coll, `"`)
 		ns += coll[:quote]
 	}
+	reslen := getDocByField(str, "reslen:")
+	resLength := 0
+	idx := strings.Index(reslen, " ")
+	if reslen != "" && idx > 0 {
+		resLength = ToInt(reslen[:])
+	}
 	stat = LogStats{filter: filter, index: index, milli: milli, ns: ns, op: op,
-		reslen: ToInt(reslen[:strings.Index(reslen, " ")]), scan: scan, utc: utc}
+		reslen: resLength, scan: scan, utc: utc}
 	return stat, nil
 }
 
